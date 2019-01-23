daffd7ef63512f5e21f141eaaa75e90f3b3770b4
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-23 01:04:19 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -8,14 +8,14 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
-  def new 
+  def new
     @rams = []
-  end 
+  end
 
-  def create 
+  def create
     dude = Student.create(first_name: params[:first_name], last_name: params[:last_name])
     @rams = params
     render "new"
-  end 
-      
+  end
+
 end
